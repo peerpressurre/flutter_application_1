5708c8bb1ddedc6cf4c92c6cb549b7d38641d270
Kotsag Sofiya <113593875+peerpressurre@users.noreply.github.com>
2023-05-16 17:08:32 +0300

    Update application.dart

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/presentation/screens/widgets/home.dart';
+// import 'package:flutter_application_1/presentation/screens/widgets/home.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home:  Scaffold(body: Home(), backgroundColor: Colors.tealAccent,),
+    return MaterialApp(
+      title: 'Personal info',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ImageWithTextOverlay(),    );
+  }
+}
+
+class ImageWithTextOverlay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal info'),
+      ),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Image.asset(
+              'assets/images/celebratingcat.png',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Positioned(
+              bottom: 16.0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                color: Colors.black.withOpacity(0.5),
+                child: Text(
+                  "Name: Sofia\nSurname: Kotsaga\nAge: 16",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
