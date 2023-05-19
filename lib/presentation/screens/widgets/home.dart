9d2e30eca5c54315bff041483b434aa9acc10233
Kotsag Sofiya <113593875+peerpressurre@users.noreply.github.com>
2023-05-20 00:02:07 +0300

    Update home.dart

--- lib/presentation/screens/widgets/home.dart
+++ lib/presentation/screens/widgets/home.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
-const String text = "Name: Sofia\nSurname: Kotsaga\nAge: 16"; 
+const String text = "Ім'я: Бетті\nПо-матері: Софіївна\nВік: 4\nДіти: 4\nГладибельність: 10/10\nПроводить більше часу: На вулиці"; 
+
 class Home extends StatelessWidget {
   const Home ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text(
-      text,
-       textDirection: TextDirection.ltr,
-        style: TextStyle(
-          color: Colors.black54,
-           fontSize: 25,
-           height: 2,
-           decorationThickness: 1.5,
-           decorationStyle: TextDecorationStyle.wavy,
-           fontStyle: FontStyle.italic
-           )),
+    return const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+          text,
+          textAlign: TextAlign.left,
+           textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.black87,
+               fontSize: 15,
+               height: 2,
+               decorationThickness: 5,
+               decorationStyle: TextDecorationStyle.wavy,
+               fontStyle: FontStyle.normal
+               )),
 );
   }
 }
-
