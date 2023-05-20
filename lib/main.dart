ee28e78e7733ef43cbed2e13b43ca2a1e85ad06f
Kotsag Sofiya <113593875+peerpressurre@users.noreply.github.com>
2023-05-20 07:36:44 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/internal/application.dart';
-import 'dart:io' show Platform;
+//import 'dart:io' show Platform;
 
 void main() {
-  if (Platform.isWindows) {
-    WidgetsFlutterBinding.ensureInitialized();
-    runApp(Application());
-  } else {
-    print('This app is only supported on Windows.');
-  }
-  // runApp(Application());
+   runApp(Application());
 }
