330651607d293f6833a04d0be258ccb933731caa
Kotsag Sofiya <113593875+peerpressurre@users.noreply.github.com>
2023-05-20 06:59:24 +0300

    Update home.dart

--- lib/presentation/screens/widgets/home.dart
+++ lib/presentation/screens/widgets/home.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
-const String text = "Ім'я: Бетті\nПо-матері: Софіївна\nВік: 4\nДіти: 4\nГладибельність: 10/10\nПроводить більше часу: На вулиці"; 
-
+import 'package:flutter_application_1/presentation/screens/widgets/widgets.dart';
+const String text1 = "Ім'я: Бетті\nПо-матері: Софіївна\nВік: 4\nДіти: 4\nГладибельність: 10/10\nПроводить більше часу: На вулиці"; 
+const String text2 = "Ім'я: Елла\nПо-матері: Софіївна\nВік: 2.5\nДіти: -\nГладибельність: 10/10\nПроводить більше часу: Дома";
 class Home extends StatelessWidget {
   const Home ({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-          text,
-          textAlign: TextAlign.left,
-           textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Colors.black87,
-               fontSize: 15,
+   Widget build(BuildContext context) {
+    return Align(
+      child: Column(children: <Widget>[
+       Stack(
+          children: const [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                text1,
+                style: TextStyle(
+                  color: Colors.black87,
+               fontSize: 13,
                height: 2,
                decorationThickness: 5,
                decorationStyle: TextDecorationStyle.wavy,
                fontStyle: FontStyle.normal
-               )),
-);
+                ),
+                ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                text2,
+                 style: TextStyle(
+                  color: Colors.black87,
+               fontSize: 13,
+               height: 2,
+               decorationThickness: 5,
+               decorationStyle: TextDecorationStyle.wavy,
+               fontStyle: FontStyle.normal
+                ),
+                ),
+            )
+          ],
+        )
+      ]
+      ,)
+    );
   }
 }
