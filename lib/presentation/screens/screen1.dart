fa38ad5bd41ebce51c3ef7b662cb959cf1121e97
Kotsag Sofiya <113593875+peerpressurre@users.noreply.github.com>
2023-05-25 15:31:43 +0300

    Update screen1.dart

--- lib/presentation/screens/screen1.dart
+++ lib/presentation/screens/screen1.dart
@@ -11,67 +11,59 @@ class Screen1 extends StatelessWidget {
         backgroundColor: Colors.purple[200],
         title: const Text('Betty'),
       ),
-      body: Center(
-        child: 
+      body: Column(
+        children: [
           Stack(
-            children:[ Padding(padding: EdgeInsets.fromLTRB(750, 250, 200, 25),
-            child:
-             Stack(
-              children:[ Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                        color: Colors.purple[200],
-                        boxShadow: [
-                 BoxShadow(
-                   color: Colors.grey.withOpacity(0.5),
-                   spreadRadius: 5,
-                   blurRadius: 7,
-                   offset: Offset(0, 3), 
-                 ),
-               ],
-                borderRadius: BorderRadius.circular(700),
-              ),
-               child: ClipOval(
-                 child: Image.network(
-                   'https://www.linkpicture.com/q/bettycropped.jpeg', 
-                   fit: BoxFit.cover,
-                 ),
-               )
-                    ),] 
-                             )
-            ),
-            
-             Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Ім'я: Бетті\nПо-матері: Софіївна\nВік: 4\nДіти: 4\nГладибельність: 10/10\nПроводить більше часу: На вулиці",
-                style: TextStyle(
-                  color: Colors.black87,
-               fontSize: 13,
-               height: 2,
-               decorationThickness: 5,
-               decorationStyle: TextDecorationStyle.wavy,
-               fontStyle: FontStyle.normal
+            children:[ 
+              Align(
+               alignment: FractionalOffset(0.5, 0.4),
+                child: 
+                Padding(padding: EdgeInsets.fromLTRB(00, 175, 50, 25),
+                child: Container(
+                      height: 175,
+                      width: 175,
+                      decoration: BoxDecoration(
+                      color: Colors.purple[200],
+                      boxShadow: [
+                BoxShadow(
+                 color: Colors.grey.withOpacity(0.5),
+                 spreadRadius: 5,
+                 blurRadius: 7,
+                 offset: Offset(0, 3), 
                 ),
+              ],
+               borderRadius: BorderRadius.circular(500),
+             ),
+              child: ClipOval(
+                child: Image.network(
+                 'https://www.linkpicture.com/q/bettycropped.jpeg', 
+                 fit: BoxFit.cover,
                 ),
-            ),       
-      ],
-      ),
-        ),
-      );   
+              )
+          )
+                )                 
+          ),
+           Align(
+            alignment: FractionalOffset(0.5, 0.4),
+                  child:
+                  Padding(padding: EdgeInsets.fromLTRB(00, 350, 50, 25),
+                  child: Text(
+                    "Ім'я: Бетті\nПо-матері: Софіївна\nВік: 4\nДіти: 4\nГладибельність: 10/10\nПроводить більше часу: На вулиці",
+                    style: TextStyle(
+                      color: Colors.black87,
+                   fontSize: 13,
+                   height: 2,
+                   decorationThickness: 7,
+                   fontWeight: FontWeight.w600,
+                   decorationStyle: TextDecorationStyle.wavy,
+                   fontStyle: FontStyle.normal
+                    ),
+                    ),
+                  ), 
+                ),       
+          ]),
+        ],
+      ),           
+          );
   }
 }
-
-
- //   Align(
-            //   alignment: Alignment.topLeft,
-            //   child: SizedBox(
-            //     // width: 200, 
-            //     height: 150, 
-            //     child: Image.network(
-            //       'https://www.linkpicture.com/q/bettycropped.jpeg',
-            //       fit: BoxFit.cover,
-            //     ),
-            //   ),
-            // ),
